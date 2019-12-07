a4b315c54834e4f6b9ab2d73b0a90b07544ac3dd
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-12-07 16:36:39 +0100

    add comments and PropsTypes verification

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+/* Not that usefull since header is a bit empty but it is when we start adding router */
 export default function Header() {
   return <div className="header">Queens playlist selector</div>;
 }
